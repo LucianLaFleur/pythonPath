347fe9a9e918ae7cb46dcb643b40f02663904b44
Lucian LaFleur <johnksummerlin@hotmail.com>
2019-12-02 04:41:01 -0500

    outline for basic clock loop made for the timerapp.

--- timerapp.py
+++ timerapp.py
@@ -1,46 +1,25 @@
 import time
 
-def countdown(n):
-    if n == 0:
-        return n
-    else:
-        print(n)
-        time.sleep(1)
-        # decrement the counter and return it as the next N val for recursive use --------
-        return countdown(n-1)
+# s, m, hr placeholders
+ts = 55
+tm = 59
+th = 1
 
-# variable for the time to countdown from
-x = 5
-print(f"Begin countdown from {x}:")
-# run the function on the var
-print(countdown(x))
+while True:
+    # zfill forces 2 digits to always appear
+    print(str(th).zfill(2)+ ":"+ str(tm).zfill(2)+":"+ str(ts).zfill(2))
+    ts += 1
+    time.sleep(1)
+    if ts == 60:
+        ts = 0
+        tm += 1
+    if tm == 60:
+        tm = 0
+        th += 1
 
-# Recursive factorial example ------------------
-def fkt(n):
-    if n == 0:
-        return 1
-    else:
-        # ex 3 x 2 x 1 is 3!
-        # same as 3 x (2 x 1) || 3 x 2!
-        # n! = n * (n-1)!
-        # recursively call n - 1 within the factorial function to achieve this calculation
-        return n * fkt(n-1)
-
-x = 5 # should return 120
-print(f"The factorial of {x} is {fkt(x)}")
-
-# Fibbonacci garbage yet again .....................................
-#ALWAYS BEGIN WRITING WITH BASE CASES
-
-def fib_recur(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib_recur(n-1) + fib_recur(n-2)
-
-x = 11
-print(f"The {x} ordinal fibbonzci number is {fib_recur(x)}")
-
-# For any number in the fib-seq > 1, fib(n) = fib(n-1) + fib(n-2)
+#  additions to construct Miss-HM's song for hour-counter
+#  westminster chimes 4 versions
+# For each hour "bong"
+#  https://www.youtube.com/watch?v=eEKUdugY6Yo 
+# Alt version with the sound of NTW firing?
+# 
